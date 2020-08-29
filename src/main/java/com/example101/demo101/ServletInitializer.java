8902daa96b33676ea551b40ae82661c26066ec85
杨志勇 <190716@lifeat.cn>
2020-08-29 12:32:58 +0800

    Initial commit

---
+++ src/main/java/com/example101/demo101/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example101.demo101;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Demo101Application.class);
+    }
+
+}
---
+++ src/test/java/com/example101/demo101/Demo101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example101.demo101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
